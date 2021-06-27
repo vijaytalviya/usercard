dcf3c587d0ce853586f6144c821d7a581b89f79c
vijay <talviyav.com>
2021-06-27 11:22:53 +0530

    new card

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,37 +2,42 @@ import React from "react";
 import "./Card.css";
 import "font-awesome/css/font-awesome.min.css";
 const Card = (props) => {
-  const { name, img, position } = props.user;
+  const { name, coverImg, position, img } = props.user;
   return (
     <div className="card">
       <div className="content">
-        <div className="img-container">
-          <img src={img} alt="img1" className="card-img-top" />
+        <div className="img-1">
+          <img src={coverImg} alt="img1" className="card-img-top" />
+          <ul>
+            <li>
+              <a href="https://www.linkedin.com">
+                <i class="fa fa-linkedin-square" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/">
+                <i class="fa fa-github" aria-hidden="true"></i>
+              </a>
+            </li>
+            <li>
+              <a href="https://twitter.com/">
+                <i class="fa fa-twitter" aria-hidden="true"></i>
+              </a>
+            </li>
+          </ul>
         </div>
+        <div className="back"></div>
         <div className="content-box">
+          <div className="img-2">
+            <img src={img} alt="img1" className="card-img-top" />
+          </div>
+
           <h4>
             {name}
             <br></br>
             <span>{position}</span>
           </h4>
         </div>
-        <ul>
-          <li>
-            <a href="#">
-              <i class="fa fa-linkedin-square" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-github" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-          </li>
-        </ul>
       </div>
     </div>
   );
--- src/data.js
+++ src/data.js
@@ -1,14 +1,18 @@
 export const user = [
   {
     id: 1,
-    name: "mickel",
+    name: "Mickel",
     position: "CEO",
-    img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80",
+    img: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80",
+    coverImg:
+      "https://which-50.com/wp-content/uploads/2018/06/iStock-486211100-1255x560.jpg",
   },
   {
     id: 2,
-    name: "jone",
+    name: "Jone",
     position: "Manager",
     img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLe5PABjXc17cjIMOibECLM7ppDwMmiDg6Dw&usqp=CAU",
+    coverImg:
+      "https://www.simplilearn.com/ice9/free_resources_article_thumb/Project-Scope-Management-Cover.jpg",
   },
 ];
